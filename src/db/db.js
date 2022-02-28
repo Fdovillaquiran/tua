aeb648b8ea8dcc430c2877e1469df63c42603bc2
fdo <fernandovillaquiran@outlook.com>
2022-02-28 15:45:04 -0500

    reconfiguracion de la arquitectura

---
+++ src/db/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const connectDB = async (db) => {
+  return await mongoose
+    .connect(db)
+    .then(() => {
+      console.log('Conection DB full')
+    })
+    .catch((err) => {
+      console.error('ERROR', err)
+    })
+}
+
+export default connectDB
